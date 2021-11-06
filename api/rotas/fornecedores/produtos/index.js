46e8345ca3a9f89d4865ba25b0b4e70a6b54a971
Marcos Antonio dos Santos <marcosantonio.dev@gmail.com>
2021-11-06 03:10:12 -0300

    add put for product

--- api/rotas/fornecedores/produtos/index.js
+++ api/rotas/fornecedores/produtos/index.js
@@ -65,4 +65,20 @@ roteador.get('/:id', async (requisicao, resposta, proximo) => {
     }
 })
 
+roteador.put('/:id', async (requisicao, resposta, proximo) => {
+    try{
+        const dados = Object.assign({}, requisicao.body, {
+            id: requisicao.params.id,
+            fornecedor: requisicao.fornecedor.id
+        })
+    
+        const produto = new Produto(dados)
+        await produto.atualizar()
+        resposta.status(204)
+        resposta.end()
+    }catch(err){
+        proximo(err)
+    }
+})
+
 module.exports = roteador
